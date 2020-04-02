4b9357ab75a7ba27c47fd2a6384d659cfd00462a
Evan Smith <professionalemail@zoho.com>
2020-04-02 06:33:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,9 @@ def long_planeteer_calls(calls_long)
   end
 end
 
-def find_the_cheese(array)
-  # the array below is here to help
+def find_the_cheese(cheese_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_array.find do |cheese|
+    cheese_types.include? 
+  end
 end

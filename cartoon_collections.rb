8d9467f832a5a8237fca18b9fdc43ae4b2f5a36c
Evan Smith <professionalemail@zoho.com>
2020-04-02 06:33:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(cheese_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_array.find do |cheese|
-    cheese_types.include? 
+    cheese_types.include?(cheese)
   end
 end

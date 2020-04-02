28fa957fcda0c21be5f76e846624d1170c76c907
Evan Smith <professionalemail@zoho.com>
2020-04-02 06:29:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,9 @@ def summon_captain_planet(planeteers)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.
+  calls_long.any do |calls|
+    calls.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
